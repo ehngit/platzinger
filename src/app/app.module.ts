b61ae672e6064ab484a9fc89f3f4c65aaf61b2e3
Edith Hdz <ehngit@gmail.com>
2020-03-04 14:37:53 -0600

    Commit components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { ConversationComponent } from './conversation/conversation.component';
+import { ContactComponent } from './contact/contact.component';
+import { MenuComponent } from './menu/menu.component';
+import { RequestComponent } from './modals/request/request.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    LoginComponent,
+    ConversationComponent,
+    ContactComponent,
+    MenuComponent,
+    RequestComponent
   ],
   imports: [
     BrowserModule,
