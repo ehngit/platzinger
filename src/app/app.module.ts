da1c8de992f6ff298cfeb48aaa1e24d07956d8e2
Edith Hdz <ehngit@gmail.com>
2020-03-04 16:30:08 -0600

    Delete files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,58 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { ProfileComponent } from './profile/profile.component';
-import { LoginComponent } from './login/login.component';
-import { ConversationComponent } from './conversation/conversation.component';
-import { ContactComponent } from './contact/contact.component';
-import { MenuComponent } from './menu/menu.component';
-import { RequestComponent } from './modals/request/request.component';
-
-import { RouterModule, Routes } from '@angular/router';
-import { AuthenticationGuard } from './services/authentication.guard';
-import { ImageCropperModule } from 'ngx-image-cropper';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-
-import {environment} from '../environments/environment';
-// 1. Import the libs you need
-import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { AngularFireStorageModule } from '@angular/fire/storage';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-
-/**
- * Router for views
- */
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent, canActivate: [AuthenticationGuard] },
-  { path: 'login', component: LoginComponent },
-  { path: 'conversation/:uid', component: ConversationComponent , canActivate: [AuthenticationGuard]},
-  { path: 'profile', component: ProfileComponent, canActivate: [AuthenticationGuard] }
-];
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    ProfileComponent,
-    LoginComponent,
-    ConversationComponent,
-    ContactComponent,
-    MenuComponent,
-    RequestComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(appRoutes),
-    ImageCropperModule,
-    NgbModule,
-    AngularFireModule.initializeApp(environment),
-    AngularFirestoreModule, // firestore
-    AngularFireAuthModule, // auth
-    AngularFireStorageModule // storage
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
