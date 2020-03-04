626b2cbbc7453483ee773ad80ec08f68efeb3f8f
Edith Hdz <ehngit@gmail.com>
2020-03-04 15:44:49 -0600

    Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyAbRybzVOLsD70ElVgllTqxlz7AZZET7gA",
+    authDomain: "platzinger-6912a.firebaseapp.com",
+    databaseURL: "https://platzinger-6912a.firebaseio.com",
+    projectId: "platzinger-6912a",
+    storageBucket: "platzinger-6912a.appspot.com",
+    messagingSenderId: "143069343580",
+    appId: "1:143069343580:web:241222a707487701799b99",
+    measurementId: "G-N7Z20QMZYT"
+  }
 };
