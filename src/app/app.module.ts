0c1894ded6a9375aa82931ba8e66adc7e84076b5
Edith Hdz <ehngit@gmail.com>
2020-03-04 16:45:17 -0600

    Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,41 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { ConversationComponent } from './conversation/conversation.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SearchPipe } from './pipes/search';
+import { FormsModule } from '@angular/forms';
+import { AuthenticationGuard } from './services/authentication.guard';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent, canActivate: [AuthenticationGuard]},
+  {path: 'login', component: LoginComponent},
+  {path: 'conversation/:uid', component: ConversationComponent},
+  {path: 'profile', component: ProfileComponent, canActivate: [AuthenticationGuard]}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-
+    ContactComponent,
+    ConversationComponent,
+    HomeComponent,
+    LoginComponent,
+    MenuComponent,
+    ProfileComponent,
+    SearchPipe,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
