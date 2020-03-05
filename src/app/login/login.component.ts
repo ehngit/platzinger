2d790b1a36835bc1cc19906e6e87629f90c399b3
Edith Hdz <ehngit@gmail.com>
2020-03-04 19:15:21 -0600

    Login with firebase

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../services/authentication.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  operation: string = 'login';
+  email: string = null;
+  password: string = null;
+  constructor(private authenticationService: AuthenticationService) { }
+
+  ngOnInit(): void {
+
+  }
+
+  login(){
+    this.authenticationService.loginWithEmail(this.email, this.password).then(
+      (data) => {
+        alert("Logueado correctamente");
+        console.log(data);
+      }).catch( (error) => {
+        alert("Ocurrió un error");
+        console.log(error);
+      });
+  }
+
+  register(){
+    this.authenticationService.registerWithEmail(this.email, this.password).then(
+      (data) => {
+        alert("Registrado correctamente");
+        console.log(data);
+      }).catch( (error) => {
+        alert("Ocurrió un error");
+        console.log(error);
+      });
+  }
+
+}
